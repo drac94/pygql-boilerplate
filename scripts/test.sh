23e4664b939a6c2ce8bef137cc6f08e02bee912f
drac94 <clovis.guerrier@gmail.com>
2020-04-11 18:48:22 -0500

    add coverage report

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,10 @@
 
 # Usage: runs tests
 
-PIPENV_DOTENV_LOCATION=./.env.test pipenv run pytest
+COVERAGE_OPTIONS="--cov-report html --cov-report term-missing:skip-covered --cov=src --cov-fail-under=85"
+RUN_TEST="PIPENV_DOTENV_LOCATION=./.env.test pipenv run pytest ${COVERAGE_OPTIONS} ${@}"
+RUN="${RUN_TEST}"
+
+eval $RUN
+
 exit "${?}"
