e9b8c3c732ce43f63a2718ea872dbd088b2dd3f0
drac94 <clovis.guerrier@gmail.com>
2020-04-11 19:05:17 -0500

    add more core tests

--- tests/core/test_post.py
+++ tests/core/test_post.py
@@ -22,3 +22,39 @@ def test_get_post_list(pg_session: Session, with_factories: None) -> None:
     posts = PostService(pg_session).get_list()
 
     assert len(posts) == BATCH_CREATE
+
+
+def test_create_post(pg_session: Session, with_factories: None) -> None:
+    post = PostService(pg_session).create("title", "content")
+
+    assert post
+
+
+def test_update_post(pg_session: Session, with_factories: None) -> None:
+
+    updated_title = "title updated"
+    updated_contnet = "content updated"
+
+    post = PostFactory()
+    updated_post = PostService(pg_session).update(
+        post.id, updated_title, updated_contnet
+    )
+
+    assert updated_post
+    assert updated_post.id == post.id
+    assert updated_post.title == updated_title
+    assert updated_post.content == updated_contnet
+
+
+def test_delete_post_by_id(pg_session: Session, with_factories: None) -> None:
+
+    post = PostFactory()
+    posts = PostService(pg_session).get_list()
+
+    assert len(posts) == 1
+
+    PostService(pg_session).delete(post.id)
+
+    posts = PostService(pg_session).get_list()
+
+    assert len(posts) == 0
